18eb89dbdc28731dfab3a86fcb54e2f43b6f5bc5
Jaya van de Pol <341509@student.mboutrecht.nl>
2026-01-21 04:47:02 +0100

    safety check for email

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -17,6 +19,7 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+
     protected $fillable = [
         'name',
         'email',
@@ -44,5 +47,22 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // For the Admin Panel (id: 'admin')
+        if ($panel->getId() === 'admin') {
+            // ONLY admins can access
+            return $this->email === 'jayavandepol@hotmail.com';
+        }
+
+        // For the User Dashboard (id: 'app')
+        if ($panel->getId() === 'dashboard') {
+            // ONLY verified users can access (optional safety check)
+            // return ! is_null($this->email_verified_at);
+            return true;
+        }
+
+        return true;
     }
 }
